89f15ac29067eb090f89ea81e80ceb75f6ff2915
deZine Ninja <vachan@kdc.in>
2022-10-04 09:06:52 +0530

    Initial Commit

---
+++ camptix-qr-checkin.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Plugin Name: CampTix QR Checkin
+ * Plugin URI:  https://wordcamp.org
+ * Description: Create a Shortcode for embeding QR Code and also enabling users with Editor Role to Checkin Attendee by scaning the QR Code.
+ * Version:     0.1.0
+ * Text Domain: tix_qrcheckin
+ * Domain Path: /languages/
+ *
+ * Author:      Vachan Kudmule
+ * Author URI:  https://dezine.ninja
+ * License:     GPLv2
+ */
+
+
+class CampTix_Addon_QR_Checkin extends CampTix_Addon {
+
+	/**
+	 * Init
+	 */
+	public function camptix_init() {
+		add_action( 'tix_qrcheckin_attendee',   array( $this, 'tix_qrcheckin_attendance' ), 10, 2 );
+		add_shortcode( 'tix_qrcheckin',         array( $this, 'tix_qrcheckin_shortcode' ) );
+	}
+
+	/**
+	 * Callback for the [camptix_attendees] shortcode.
+	 */
+	public function tix_qrcheckin_shortcode( $attr ) {
+		$attendee_id = $attr['attendee_id'];
+        $attendee = get_post( $attendee_id );
+
+		$attr = $this->sanitize_attendees_atts( $attr );
+
+		return $this->get_attendees_shortcode_content( $attr );
+	}
+
+}
+
+// Register this class as a CampTix Addon.
+camptix_register_addon( 'CampTix_Addon_QR_Checkin' );
+ 
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== CampTix QR Checkin ===
+Contributors:      vachan, kdclabs, kdctek
+Tags:              ticketing, event ticketing, qr, qrcode, checkin, attendee checkin
+Requires at least: 3.5
+Tested up to:      6.0
+Stable tag:        0.1.0
+Donate link:       http://wordpressfoundation.org/donate/
+License:           GPLv2 or later
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+Create a Shortcode for embeding QR Code and also enabling users with Editor Role to Checkin Attendee by scaning the QR Code.
+
+== Description ==
+
+Create a Shortcode for embeding QR Code and also enabling users with Editor Role to Checkin Attendee by scaning the QR Code.
+
+== Installation ==
+
+1. Download and extract CampTix QR Checkin in your `wp-content/plugins` directory
+1. Activate the plugin through the Plugins menu in WordPress
+1. Create a ticket or two, place the `[tix_qrcheckin]` shortcode in any camptix email template
+
+== Changelog ==
+
+= 0.1.0 (2022-10-04) =
+* Initial Commit.
+* Just the bear bones.
